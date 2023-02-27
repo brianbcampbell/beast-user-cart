17f8dcf22348e2a5e17d6111bf08270e5621f12d
bc <52678934+brianbcampbell@users.noreply.github.com>
2023-02-26 22:23:18 -0700

    initial commit

---
+++ src/main/java/beast/cart/CartApplication.java
@@ -0,0 +1,13 @@
+package beast.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class CartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartApplication.class, args);
+	}
+
+}
